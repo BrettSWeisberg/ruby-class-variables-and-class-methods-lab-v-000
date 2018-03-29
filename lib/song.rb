17dbac0a283fc595a503b1dd72e4f153a5f63fd6
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-29 00:17:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,40 @@
 
 class Song
   attr_accessor :name, :artist, :genre
+  attr_reader :artists, :genres
 
   @@count = 0
-  @@artist = []
-  @@genres = []
+  @@genre = []
+
+  def new(name,artist,genre)
+  end
 
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-
-    @@artists << @artist
+    @@artists = {}
+    @@genres = {}
   end
 
   def self.count
      @@count
   end
 
-  def self.artists
-    @@artists
+  def self.genres
+    if @@genres.count(artist) == 0
+     @@genres << self
+   end
   end
 
-  def self.genres
-    @@genre
+  def artists(artist)
+    array_artists = []
+    if array_artists.count(artist) == 0
+      array_artists << artist
+    end
   end
+
 
 
 
